9a684f10b244c61492c4a54928babbe183edb1bb
Sergey Yanenko <syanenko65@gmail.com>
2024-04-03 17:25:54 +0300

    Call povray

--- render/render_pov.php
+++ render/render_pov.php
@@ -12,5 +12,13 @@ if(!is_dir($UPLOADS_DIR))
 $model_path = $UPLOADS_DIR . $_FILES['scene']['name'];
 move_uploaded_file($_FILES['scene']["tmp_name"], $model_path);
 
-echo json_encode($model_path); // Change to image after rendering
+$pov_in   =  '/home/s/syanenzc/povlab.online/public_html/render/scenes/test/sphere.pov';
+$pov_out  = $UPLOADS_DIR . 'scene.png';
+$pov_keys = ' -W640 -H480 ';
+$command='./povray -i' . $pov_in . ' -o' . $pov_out . $pov_keys . '2>&1';
+// echo json_encode($command);
+$output=null;
+$retval=null;
+exec($command, $output, $retval);
+echo json_encode($pov_out);
 ?>
