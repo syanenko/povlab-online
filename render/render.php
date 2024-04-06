918394532f0852e062e16043ee543c1c2bc74548
Sergey Yanenko <syanenko65@gmail.com>
2024-04-06 07:48:33 +0300

    Control alpha by background

--- render/render.php
+++ render/render.php
@@ -15,10 +15,6 @@ move_uploaded_file($_FILES['scene']["tmp_name"], $model_path);
 $pov_in   = $UPLOADS_DIR . $_FILES['scene']['name'];
 $pov_out  = $UPLOADS_DIR . 'scene.png';
 $pov_keys = ' hgpovray/etc/povray/3.8/povray.ini ';
-if($_GET['alpha'] == 1)
-{
-  $pov_keys = $pov_keys . '+UA ';
-}
 
 // $command='/home/s/syanenzc/povlab.online/public_html/render/hgpovray/bin/povray -i' . $pov_in . ' -o' . $pov_out . $pov_keys . '2>&1';
 $command='hgpovray/bin/povray -i' . $pov_in . ' -o' . $pov_out . $pov_keys . '2>&1';
