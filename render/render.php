00d66b9568e2f4b7d90f236b03365b6784e348cf
Sergey Yanenko <syanenko65@gmail.com>
2024-04-06 03:00:03 +0300

    Alpha control

--- render/render.php
+++ render/render.php
@@ -14,11 +14,22 @@ move_uploaded_file($_FILES['scene']["tmp_name"], $model_path);
 
 $pov_in   = $UPLOADS_DIR . $_FILES['scene']['name'];
 $pov_out  = $UPLOADS_DIR . 'scene.png';
-$pov_keys = ' -W1024 -H768 +A0.3 +J +R4 ';
-$command='./povray -i' . $pov_in . ' -o' . $pov_out . $pov_keys . '2>&1';
+$pov_keys = ' /home/s/syanenzc/povlab.online/public_html/render/hgpovray/etc/povray/3.8/povray.ini ';
+if($_GET['alpha'] == 1)
+{
+  $pov_keys = $pov_keys . '+UA ';
+}
+
+$command='/home/s/syanenzc/povlab.online/public_html/render/hgpovray/bin/povray -i' . $pov_in . ' -o' . $pov_out . $pov_keys . '2>&1';
+// echo json_encode($_GET);
 // echo json_encode($command);
+// $command='pwd 2>&1';
+
 $output=null;
 $retval=null;
 exec($command, $output, $retval);
+//echo json_encode($retval);
+//echo json_encode($output);
+
 echo json_encode($pov_out);
 ?>
--- render/render_test.sh
+++ render/render_test.sh
@@ -2,4 +2,6 @@
 #povray -iscenes/1TQN/1TQN_POV_scene.pov -o./images/1TQN_1920x1080_aa.png -W1920 -H1080 +A0.0 +J +R4
 #povray -iscenes/1TQN/1TQN_POV_scene.pov -o./images/1TQN_640x480_noaa.png -W640 -H480
 #./ivcon ./scenes/test/1TQN.obj ./scenes/test/1TQN.pov
-povray -iscenes/test/1TQN.pov -o./scenes/test/1TQN_1920x1080_aa.png -W1920 -H1080 +A0.0 +J +R4
+# povray -iscenes/test/1TQN.pov -o./scenes/test/1TQN_1920x1080_aa.png -W1920 -H1080 +A0.0 +J +R4
+#povray -iscenes/1TQN/1TQN_POV_scene.pov -o./scenes/1TQN/1TQN_1920x1080_aa.png -W1920 -H1080 +A0.0 +J +R4
+povray -iscenes/1TQN/1TQN_POV_scene.pov -o./scenes/1TQN/1TQN.png -W1920 -H1080 +A0.0 +J +R4
